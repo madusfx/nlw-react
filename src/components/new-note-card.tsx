063a1bb281052e6a39720cc9a12ec20c1926733f
madusfx <mariaesfreitas@gmail.com>
2024-02-06 00:19:15 -0300

    finishing the initial page and his style

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,11 +1,11 @@
 export function NewNoteCard() {
   return (
-    <div className="rounded-md bg-slate-700 p-5 space-y-3 overflow-hidden relative">
+    <button className="rounded-md text-left outline-none bg-slate-700 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
       <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
       <p className="text-sm leading-6 text-slate-400">
         Grave uma nota em áudio que será convertida para texto automaticamente.
       </p>
       <div className="absolute bottom-0 left-0 right-0 h-1/2" />
-    </div>
+    </button>
   );
 }
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
   return (
-    <div className="rounded-md bg-slate-700 p-5 space-y-3 overflow-hidden relative">
+    <button className="rounded-md text-left outline-none bg-slate-700 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
       <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
       <p className="text-sm leading-6 text-slate-400">
         Grave uma nota em áudio que será convertida para texto automaticamente.
@@ -11,6 +11,6 @@ export function NoteCard() {
         automaticamente.
       </p>
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </div>
+    </button>
   );
 }
