20356844a9beb680efa0d822399fba9635325b07
madusfx <mariaesfreitas@gmail.com>
2024-02-07 11:51:50 -0300

    styling and finishing the note card component

--- src/app.tsx
+++ src/app.tsx
@@ -19,7 +19,7 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard />
+        <NoteCard note={{ date: new Date(), content: 'Teste' }} />
       </div>
     </div>
   );
