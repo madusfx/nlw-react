6cdd260b4e6f306d9bdd245368832633c5646510
madusfx <mariaesfreitas@gmail.com>
2024-02-08 15:11:15 -0300

    adding responsive style to the page and components

--- src/app.tsx
+++ src/app.tsx
@@ -38,13 +38,12 @@ export function App() {
   const filteredNotes =
     search !== ''
       ? notes.filter(note =>
-        // eslint-disable-next-line prettier/prettier
         note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
       )
       : notes;
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NLW Expert" />
 
       <form className="w-full">
@@ -58,7 +57,7 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {filteredNotes.map(note => {
